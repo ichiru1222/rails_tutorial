83fbd784696a3901bfae751df6f73fa9912ae48a
ichiru <ichiru1222@gmail.com>
2022-05-07 18:17:26 +0900

    Make a basic Usermodel(including secure passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,21 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+      password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
